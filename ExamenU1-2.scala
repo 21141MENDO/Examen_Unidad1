14765f6530d4f212b5cee5d52ffe9d7bb419c070
Your Name <you@example.com>
2019-03-05 18:09:57 -0800

    Eu1

---
+++ ExamenU1-2.scala
@@ -0,0 +1,55 @@
+1// Comienza una simple Spark Session
+import org.apache.spark.sql.SparkSession
+val spar = SparkSession.builder().getOrCreate()
+
+2// Cargue el archivo Netflix Stock CSV, haga que Spark infiera los tipos de datos
+val df = spark.read.option("header", "true").option("inferSchema","true")csv("Netflix_2011_2016.csv")
+
+3// ¿Cuáles son los nombre de las columnas?
+df.columns
+
+4// ¿Cómo es el esquema?
+df.printSchema()
+
+5// Imprime las primeras 5 columnas
+df.select("Date","Open","High","Low","Close","Volume","Adj Close").show(5)
+df.show(5)
+
+6// Usa describe() para aprender sobre el DataFrame
+df.describe().show()
+
+7// Crea un nuevo dataframe con una columna llamada "HV Ratio" que es la relación entre precio de la columna "High" frente a columna "volumen" de acciones negociadas por un día.
+val df2 = df.withColumn("HV Ratio",df("High")+df("Volume"))
+df2.printSchema()
+df2.show()
+
+8// ¿Que dia tuvo el pico mas alto en la columna "High" ?
+df.orderBy($"High".desc).show()
+
+9// ¿Cuál es el significado de la columna Cerrar "Close"?
+println("Son los valores de cierre de las acciones de Netflix durante los dias analizados")
+
+10// ¿Cuál es el máximo y mínimo de la columna "Volumen"?
+df.select(min(df("Volume"))).show()
+df.select(max(df("Volume"))).show()
+
+11// Con Sintaxis Scala/Spark $ conteste lo siguiente:
+//a ¿Cuántos días fue columna "Close" inferior a $600?
+df.filter($"Close"<600).count()
+
+//b ¿Qué porcentaje del tiempo fue la columna "High" mayor que $500?
+(df.filter($"High">500).count() * 1.0 / df.count()) * 100
+
+//c ¿Cuál es la correlación de Pearson entre la columna "High" y la columna "Volumen"?
+df.select(corr("High","Volume")).show()
+
+//d ¿Cuál es el máximo de la columna "HIgh"  por año?
+val dfyear = df.withColumn("Year",year(df("Date")))
+val maxyear = dfyear.select($"Year", $"High").groupBy("Year").max()
+val res = maxyear.select($"Year", $"max(High)")
+res.show()
+maxyear.show()
+//e ¿Cuál es el promedio de la columna "close" para cada mes del calendario?
+val dfmonth = df.withColumn("Month",month(df("Date")))
+val avgmonth = dfmonth.select($"Month",$"Close").groupBy("Month").mean()
+avgmonth.select($"Month",$"avg(Close)").orderBy("Month").show()
--- ExamenU1.scala
+++ ExamenU1.scala
@@ -1,10 +1,13 @@
-def isEven(num:Int): Boolean = {
+//1 Verifique solo numero par
+// a. escriba una funcion que tome un entero y devuelva un boleano que indique si es par o no.
+def espar(num:Int): Boolean = {
 return num%2 == 0
 }
-def isEven(num:Int): num%2 == 0
-println(isEven(6))
-println(isEven(3))
+println(espar(4))
+println(espar(7))
 
+//2. Buscar numeros pares en lista.
+//a. Escriba una funcion que devuelva True si hay un numero par dentro de una Lista: de lo contrario devuelva False.
 def numeroPar(list:List[Int]): String ={
     for(n <- list){
         if(n%2==0){
@@ -15,14 +18,15 @@ def numeroPar(list:List[Int]): String ={
     }
     return "echo"
 }
-
+//valores para mi lista
 val l1 = List(1,2,3,4,5,6,7,8)
 val l2 = List(4,3,22,55,7,8)
+//comprobando valores
 numeroPar(l1)
 numeroPar(l2)
 
-//3 7 afortunado
-
+//3. Afortunado numero 7.
+//a. Tome una lista de enteros y calcule su suma. Sin embargo, los sietes tienen suerte y deben contarse dos veces, lo que significa que su valor es 14 para la suma.
 def afortunado(list:List[Int]): Int={
     var res=0
     for(n <- list){
@@ -35,9 +39,11 @@ def afortunado(list:List[Int]): Int={
     return res
 }
 
-val af= List(1,7,7)
-println(afortunado(af))
+val saf= List(1,6,7)
+println(afortunado(saf))
 
+//4. Puede equilibrar?.
+//a. Dada una lista no vacia de enteros, devuelva verdadero si hay un lugar para dividir la lista de modo que la suma de los numeros en un lado sea igual a la suma de los numeros en el otro lado.
 def balance(list:List[Int]): Boolean={
     var primera = 0
     var segunda = 0
@@ -62,15 +68,20 @@ val bl3 = List(10,30,90)
 balance(bl)
 balance(bl2)
 balance(bl3)
-
+//5. Verificar palindromo:
+//a. Dada una cadena, devuelva un boleano que indica si es o no un palindromo.
 def palindromo(palabra:String):Boolean ={
     return (palabra == palabra.reverse)
 }
 
-val palabra = "OSO"
+val palabra = "OXXO"
 val palabra2 = "ANNA"
-val palabra3 = "JUAN"
+val palabra3 = "PEDRO"
 
 println(palindromo(palabra))
 println(palindromo(palabra2))
 println(palindromo(palabra3))
+
+
+
+
